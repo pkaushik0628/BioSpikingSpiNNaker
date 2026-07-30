c925ddde0a77c4c0a02a0d6b01f56e624fd71548
Padmanabh Kaushik <pkaushik0628@gmail.com>
2026-07-30 14:43:44 +1000

    Update bio_spiking_symmetric_stdp.h

--- timing_dependence/bio_spiking_symmetric_stdp.h
+++ timing_dependence/bio_spiking_symmetric_stdp.h
@@ -3,13 +3,16 @@
 
 #include <neuron/plasticity/stdp/synapse_structure/synapse_structure_weight_impl.h>
 #include "timing.h"
-#include <neuron/plasticity/stdp/weight_dependence/weight_additive_one_term_impl.h>
+#include <neuron/plasticity/stdp/weight_dependence/weight_one_term.h>
 #include <debug.h>
 #include <neuron/plasticity/stdp/maths.h>
 #include <neuron/plasticity/stdp/stdp_typedefs.h>
 
 // External Variables
 extern int16_lut *tau_symm_lookup;
+extern double *symm_amp; 
+extern double *learning_rate;
+extern uint16_t *invtau_symm;
 
 /**
 Creates a struct to store a symmetric trace
@@ -19,7 +22,9 @@ typedef struct symm_trace_t {
     uint32_t last_spike_time;
 } symm_trace_t;
 
-
+/**
+Initializes a symmetric trace. Sts the trace to 0
+ */
 static inline symm_trace_t initialize_symm_trace(void){
     return (symm_trace_t){
         .trace = 0
@@ -27,7 +32,11 @@ static inline symm_trace_t initialize_symm_trace(void){
 }
 
 /**
-Updates the post-synaptic trace: performs exponential decay and increments the trace in case of spike
+Updates the post-synaptic trace: performs exponential decay to post synaptic trace
+@param current time current simulation time
+@param last_time last time of a recorded spike
+@param last_trace trace to be updated over dt = time - last_time
+@return decayed/updated symmetric trace
  */
 static inline symm_trace_t timing_decay_post(uint32_t current_time, uint32_t last_time, symm_trace_t last_trace){
     uint32_t dt = current_time - last_time;
@@ -35,28 +44,68 @@ static inline symm_trace_t timing_decay_post(uint32_t current_time, uint32_t las
     return (symm_trace_t) {.trace = decayed_trace, .last_spike_time = last_trace.last_spike_time};
 }
 
-
+/**
+Updates the pre-synaptic trace: performs exponential decay to pre synaptic trace
+@param current time current simulation time
+@param last_time last time of a recorded spike
+@param last_trace trace to be updated over dt = time - last_time
+@return decayed/updated symmetric trace
+ */
 static inline symm_trace_t timing_decay_pre(uint32_t current_time, uint32_t last_time, symm_trace_t last_trace){
     uint32_t dt = current_time - last_time;
     int32_t decayed_trace = STDP_FIXED_MUL_16X16(last_trace.trace, maths_lut_exponential_decay(dt, tau_symm_lookup));
     return (symm_trace_t) {.trace = decayed_trace, .last_spike_time = last_trace.last_spike_time};
 }
 
-static inline symm_trace_t timing_add_post_spike(uint32_t time, uint32_t last_time, symm_trace_t last_trace, uint16_t invtau_symm){
+/**
+Adds the impact of post spike to STDP trace: performs exponential decay to post synaptic trace and increments the STDP trace
+@param time current simulation time
+@param last_time last time of a recorded spike
+@param last_trace trace to be updated over dt = time - last_time
+@return decayed/updated post trace
+@math U_post = U_post*exp(-dt/tau) + invtau, where U_post is post_synaptic trace
+ */
+static inline symm_trace_t timing_add_post_spike(uint32_t time, uint32_t last_time, symm_trace_t last_trace){
     symm_trace_t temp = timing_decay_post(time, last_time, last_trace);
     temp.trace = temp.trace + invtau_symm;
     temp.last_spike_time = time;
     return temp;
 }
 
-static inline symm_trace_t timing_add_pre_spike(uint32_t time, uint32_t last_time, symm_trace_t last_trace, uint16_t invtau_symm){
+/**
+Adds the impact of a pre-spike to STDP trace: performs exponential decay to pre synaptic trace and increments the STDP trace
+@param time current simulation time
+@param last_time last time of a recorded spike
+@param last_trace trace to be updated over dt = time - last_time
+@return decayed/updated pre trace
+@math U_pre = U_pre*exp(-dt/tau) + invtau, where U_pre is pre_synaptic trace
+ */
+static inline symm_trace_t timing_add_pre_spike(uint32_t time, uint32_t last_time, symm_trace_t last_trace){
     symm_trace_t temp = timing_decay_pre(time, last_time, last_trace);
     temp.trace = temp.trace + invtau_symm;
     temp.last_spike_time = time;
     return temp;
 }
 
-static inline update_state_t timing_apply_pre_spike(uint32_t time, UNUSED uint32_t last_pre_time, UNUSED symm_trace_t last_pre_trace, uint32_t last_post_time, symm_trace_t last_post_trace, update_state_t previous_state, double symm_amp, double learning_rate){
+/**
+This model supports potentiation only STDP
+       dW = A_sym*eta*U_post(t-dt)*exp(-dt/tau) if t_pre > t_post (i.e. triggered in pre-syn spike)
+    or dW = A_sym*eta*U_pre(t-dt)*exp(-dt/tau) if t_post > t_pre (i.e. triggered on post-syn spike)
+ */
+
+/**
+Called on pre-spike: Updates STDP state by calculating weight updates (technically depression)
+Note: We use weight_one_term_apply_potentiation() because this is potentiation only STDP
+@param time current time t
+@param pre_trace current pre trace
+@param last_pre_time the previous time of pre-spike
+@param last_pre_trace the previous pre-syn trace
+@param last_post_time the previous time of post-spike
+@param last_post_trace the previous post-syn trace
+@param previous_state the state to be updated
+@math dW = A_sym*eta*U_post(t-dt)*exp(-dt/tau)
+ */
+static inline update_state_t timing_apply_pre_spike(uint32_t time, UNUSED symm_trace_t pre_trace, UNUSED uint32_t last_pre_time, symm_trace_t last_pre_trace,  uint32_t last_post_time, symm_trace_t last_post_trace, update_state_t previous_state){
     uint32_t dt = time - last_post_time;
     int32_t decayed_post = STDP_FIXED_MUL_16X16(last_post_trace.trace, maths_lut_exponential_decay(dt, tau_symm_lookup));
     double gain = (double)(symm_amp * learning_rate);
@@ -64,11 +113,21 @@ static inline update_state_t timing_apply_pre_spike(uint32_t time, UNUSED uint32
     return weight_one_term_apply_potentiation(previous_state, (int32_t) val);  
 }
 
-static inline update_state_t timing_apply_post_spike(uint32_t time, uint32_t last_pre_time, symm_trace_t last_pre_trace, UNUSED uint32_t last_post_time, UNUSED symm_trace_t last_post_trace, update_state_t previous_state, double symm_amp, double learning_rate){
+/**
+Called on post spike: Updates STDP state by calculating weight updates (technically potentiation)
+@param time current time t
+@param post_trace current post trace
+@param last_pre_time timing of last pre-syn spike
+@param last_pre_trace last/un-updated pre-syn trace
+@param last_post_time the previous time of post-spike
+@param last_post_trace the previous post-syn trace
+@param previous_state the state to be updated
+@math dW = A_sym*eta*U_pre(t-dt)*exp(-dt/tau)
+ */
+static inline update_state_t timing_apply_post_spike(uint32_t time, UNUSED symm_trace_t post_trace, uint32_t last_pre_time, symm_trace_t last_pre_trace, UNUSED uint32_t last_post_time, UNUSED symm_trace_t last_post_trace, update_state_t previous_state){
     uint32_t dt = time - last_pre_time;
     int32_t decayed_pre = STDP_FIXED_MUL_16X16(last_pre_trace.trace, maths_lut_exponential_decay(dt, tau_symm_lookup));
     double gain = (double)(symm_amp * learning_rate);
     double val = gain*(double)decayed_pre;
-    return weight_one_term_apply_potentiation(previous_state, (int32_t)val); 
+    return weight_one_term_apply_potentiation(previous_state, (int32_t) val); 
 }
-#endif
